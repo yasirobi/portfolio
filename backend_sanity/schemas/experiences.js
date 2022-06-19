0488ae8a8c705de30fd562c81f6407f9d61a3a67
yasirobi <yasirtotakhil@gmail.com>
2022-06-19 18:09:49 +0200

    initial commit

---
+++ backend_sanity/schemas/experiences.js
@@ -0,0 +1,25 @@
+export default{
+    name:'experiences',
+    title:'Experiences',
+    type: 'document',
+    fields:[
+        {
+            name:'year',
+            title:'Year',
+            type:'string'
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'string'
+        },
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+    ]
+}
